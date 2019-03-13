653d0c8baaec902e7a04f61dc7845fd55a702d2b
Stanislav Yeshchenko <yeshchenkostanislav@gmail.com>
2019-03-13 19:42:39 +0100

    Правки по таймеру

--- js/script.js
+++ js/script.js
@@ -87,20 +87,27 @@ window.addEventListener('DOMContentLoaded', function () {
 
 let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
   V = 0.5; // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+
 for (var i = 0; i < linkNav.length; i++) {
-  linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
-    e.preventDefault(); //отменяем стандартное поведение
-    var w = window.pageYOffset, // производим прокрутка 
+  linkNav[i].addEventListener('click', function (event) { //по клику на ссылку
+
+    event.preventDefault(); //отменяем стандартное поведение
+
+    let w = window.pageYOffset, // производим прокрутка 
       hash = this.href.replace(/[^#]*(.*)/, '$1'); // к id элемента, к которому нужно перейти
     t = document.querySelector(hash).getBoundingClientRect().top, // отступ от окна браузера до id
       start = null;
+
     requestAnimationFrame(step);
 
     function step(time) {
+
       if (start === null) start = time;
-      var progress = time - start,
+      let progress = time - start,
         r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+
       window.scrollTo(0, r);
+
       if (r != w + t) {
         requestAnimationFrame(step)
       } else {
